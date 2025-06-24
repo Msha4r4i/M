4c8e2aa044cf13e20b1e4eb69fa9344573c5ace5
Msha4r4i <mshari.9420@gmail.com>
2025-06-24 20:25:22 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-      /*  System.out.println(" Weight (kg) =");  //الوزن
+
+      /*
+       Scanner scanner = new Scanner(System.in);
+       System.out.println(" Weight (kg) =");  //الوزن
         double kg = scanner.nextDouble();
 
         System.out.println("height (m) = ");  //اضف الطول
@@ -15,13 +17,25 @@ public class Main {
         double bmi = kg / (m * m) ;  //حسابة كتلة الجسم
         System.out.println("BMI = " + bmi); */
 
-         /* System.out.println("Obtained mark ="); //الدرجه
-        double mark =scanner.nextDouble();
-        double total = 100; //مجموع الدرجات
-        double percentage = (mark / total) * 100;
-        System.out.println("percentage  ="+ percentage ); */
 
-         /* System.out.println(" Amount in USD :");
+
+       /* Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter obtained marks: ");
+        double obtainedMarks = scanner.nextDouble();
+
+        System.out.print("Enter total marks: ");
+        double totalMarks = scanner.nextDouble();
+
+        double percentage = (obtainedMarks / totalMarks) * 100;
+
+        System.out.printf("Percentage = %.1f%%", percentage);
+    }
+} */
+
+         /*
+         Scanner scanner = new Scanner(System.in);
+         System.out.println(" Amount in USD :");
         Double usd = scanner.nextDouble();
 
         System.out.println("Exchange Rate (USD to EUR)");
@@ -30,43 +44,101 @@ public class Main {
         scanner.nextLine();
         System.out.println("Amount in EUR = "+ eur); */
 
-       /* System.out.println("Sentence :");
+
+        /*  Scanner scanner = new Scanner(System.in);
+        System.out.println("Sentence :");
         String Sentence = scanner.nextLine();
 
         System.out.println("Start index :");
         int start = scanner.nextInt();
 
         System.out.println("end index :");
-        int end =scanner.nextInt();
+        int end = scanner.nextInt();
 
         String result = Sentence.substring(start, end);
-        System.out.println(result); */
+        System.out.println(result);
+    }
+} */
 
+       /*  Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a sentence:");
+        String sentence = scanner.nextLine();
+
+        System.out.print("Enter start index: ");
+        int startIndex = scanner.nextInt();
+
+        System.out.print("Enter end  index: ");
+        int endIndex = scanner.nextInt();
+
+        String result = sentence.substring(startIndex, endIndex);
+       System.out.println(result);
+
+    }
+} */
+
+       /* Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence = scanner.nextLine();
+
+        System.out.print("Enter a keyword: ");
+        String keyword = scanner.nextLine();
+
+        if (sentence.contains(keyword)) {
+            System.out.println("Keyword \"" + keyword + "\" is present in the sentence.");
+        } else {
+            System.out.println("Keyword \"" + keyword + "\" is not present in the sentence.");
+        }
+    }
+} */
+
+       /* Scanner scanner = new Scanner(System.in);
+
+        System.out.print("sentence: ");
+        String sentence = scanner.nextLine();
+
+        System.out.print("Replace= ");
+        String wordToReplace = scanner.nextLine();
+
+        System.out.print("Replacement Word = ");
+        String replacementWord = scanner.nextLine();
+
+        String modifiedSentence = sentence.replace(wordToReplace, replacementWord);
+
+        System.out.println("Modified sentence: " + modifiedSentence);
+    }
+} */
+
+
+
+
+       /* Scanner scanner = new Scanner(System.in);
         System.out.println("String 1");
         String str1 = scanner.nextLine();
 
         System.out.println("String 2");
         String str2 = scanner.nextLine();
-        if (str1.equalsIgnoreCase(str2)){
+        if (str1.equalsIgnoreCase(str2)) {
             System.out.println("Strings are aqual (ignoring case).");
-        }else {
+        } else {
             System.out.println("Strings are Not aqual");
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
