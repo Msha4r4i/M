8b91289892e28f1d8b06d257c429f7b61183d542
Msha4r4i <mshari.9420@gmail.com>
2025-06-26 14:57:15 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,129 +4,281 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println(" ibute first number 1 :");
+//        int num1 = scanner.nextInt();
+//        System.out.println(" inbut secont number 2 :");
+//        int num2 = scanner.nextInt();
+
+//
+//        int collection = num1 + num2;
+//        int Subtraction = num1 - num2;
+//        int m = num1 * num2;
+//        int portion = num1 / num2;
+//        int n = num1 % num2;
+//
+//        System.out.println(num1 + "+" +num2+ "=" + collection);
+//        System.out.println(num1 + "-" +num2+ "=" + Subtraction);
+//        System.out.println(num1 + "*" +num2+ "=" + m);
+//        System.out.println(num1 + "/" +num2+ "=" + portion);
+//        System.out.println(num1 + "%" +num2+ "=" + collection);
+//
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println(" Enter the number :");
+//        int number = scanner.nextInt();
+//
+//
+//        for (int i = 1; i <=10; i++){
+//            System.out.print(number + "*" + i+"=");
+//            System.out.println((number* i));
+//
+//        }
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int num1;
+//        int num2;
+//        int num3;
+//        int num4;
+//        int num5;
+//
+//
+//        while (true){
+//            System.out.println("number 1");
+//            int num = scanner.nextInt();
+//            System.out.println("number 2 ");
+//            int num2 = scanner.nextInt();
+//            System.out.println("number 3");
+//            int num3 =scanner.nextInt();
+//            System.out.println(" number 4 :");
+//            int num4 = scanner.nextInt();
+//            System.out.println("number 5");
+//            int num5 = scanner.nextInt();
+//
+//
+//
+//
+//
+//        }
+//
+//        }
+//    }
+
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println(" Input the first number :");
+//        int num1 = scanner.nextInt();
+//
+//        System.out.println("Input the second number :");
+//        int num2 = scanner.nextInt();
+//
+//        System.out.println(" Final output :");
+//        int a = num1 + num2;
+//
+//        if (a == scanner.nextInt()) {
+//            System.out.println("true");
+//        } else {
+//            System.out.println("false");
+//        }
+//    }
+//}
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Input the word :");
+//        String word = scanner.nextLine();
+//
+//        String Reverse = "";
+//
+//        for (int i = word.length() - 1; i >= 0; i--) {
+//            Reverse += word.charAt(i);
+//        }
+//        System.out.println(Reverse);
+//    }
+//}
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Enter the number :");
+//        int num1 = scanner.nextInt();
+//
+//        if (num1 % 2 != 0) {
+//            System.out.println(num1);
+//            System.out.println(" The number is odd ");
+//        } else {
+//            System.out.println(num1);
+//            System.out.println(" The number is even :");
+//        }
+//    }
+//}
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println(" Enter temperature in Centigrade ");
+//        int tm = scanner.nextInt();
+//        double f = 1.8;
+//        double m = 32;
+//
+//        double fah = (tm * f) + m;
+//        System.out.println(fah);
+//    }
+//}
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println(" Enter a String :");
+//        String str = scanner.next();
+//
+//        System.out.println(" Enter a number :");
+//        int num = scanner.nextInt();
+//
+//        if (num >= 0 && num < str.length()) {
+//            System.out.println( num + str.charAt(num));
+//        } else {
+//            System.out.println("t");
+//        }
+//        scanner.close();
+//    }
+//}
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println(" Enter  the Width : ");
+//        double width = scanner.nextDouble();
+//
+//        System.out.println(" Enter the Height :");
+//        double height = scanner.nextDouble();
+//
+//        double area = width * height;
+//        double perimeter = 2 * (width + height);
+//
+//        System.out.println(" Area is  " + width + " * " + height +" = "+ area);
+//        System.out.println(" perimeter is 2* " + width + " + " + height + " = " + perimeter);
+//
+//
+//    }
+//}
+
+//        Scanner scanner = new Scanner(System.in);
+
+//        System.out.println(" Enter first number :");
+//        int num1 = scanner.nextInt();
+//
+//        System.out.println(" Enter second number ");
+//        int num2 = scanner.nextInt();
+//
+//        if (num1 == num2) {
+//            System.out.println(" t ");
+//        } else if (num1 < num2) {
+//            System.out.println("t");
+//        } else if (num1 <= num2) {
+//            System.out.println("t");
+//        } else {
+//            System.out.println("F");
+//        }
+//    }
+//}
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println(" Enter a sacent :");
+//        int s = scanner.nextInt();
+//
+//        int min = s / 60;
+//        int hour = min / 60;
+//
+//        System.out.println(hour + " : " + min + " : " + s);
+//    }
+//}
+
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println(" first number : ");
+//        int num1 = scanner.nextInt();
+//
+//        System.out.println(" second number : ");
+//        int num2 = scanner.nextInt();
+//
+//        System.out.println(" third number :");
+//        int num3 = scanner.nextInt();
+//
+//        System.out.println(" fouurh number : ");
+//        int num4 = scanner.nextInt();
+//
+//        if (num1 == num2 && num2 == num3 && num3 == num4) {
+//            System.out.println(" true ");
+//        } else {
+//            System.out.println(" Numbers are not equal! ");
+//        }
+//    }
+//}
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println(" enter the number : ");
+//        int num = scanner.nextInt();
+//
+//        if (num == 0) {
+//            System.out.println(" the number is 0 ");
+//        } else if (num < 0) {
+//            System.out.println(num + "  number is -");
+//        } else {
+//            System.out.println(num + " Number is positive ");
+//        }
+//    }
+//}
+
+        Scanner scanner = new Scanner(System.in);
+        int m;
+        int b;
+        int a;
+
+        System.out.println(" enter a number :");
+        int num = scanner.nextInt();
 
 
-      /*
-       Scanner scanner = new Scanner(System.in);
-       System.out.println(" Weight (kg) =");  //الوزن
-        double kg = scanner.nextDouble();
-
-        System.out.println("height (m) = ");  //اضف الطول
-        double m = scanner.nextDouble();
-
-        double bmi = kg / (m * m) ;  //حسابة كتلة الجسم
-        System.out.println("BMI = " + bmi); */
+            if (num == 0) {
+                int m = scanner.nextInt();
+                System.out.println(m);
 
 
+            } else if (num < 0) {
+                System.out.println(num);
 
-       /* Scanner scanner = new Scanner(System.in);
+            } else if( num > 0) {
+                System.out.println(num);
 
-        System.out.print("Enter obtained marks: ");
-        double obtainedMarks = scanner.nextDouble();
-
-        System.out.print("Enter total marks: ");
-        double totalMarks = scanner.nextDouble();
-
-        double percentage = (obtainedMarks / totalMarks) * 100;
-
-        System.out.printf("Percentage = %.1f%%", percentage);
-    }
-} */
-
-         /*
-         Scanner scanner = new Scanner(System.in);
-         System.out.println(" Amount in USD :");
-        Double usd = scanner.nextDouble();
-
-        System.out.println("Exchange Rate (USD to EUR)");
-        double rate = scanner.nextDouble();
-        double eur = usd * rate;
-        scanner.nextLine();
-        System.out.println("Amount in EUR = "+ eur); */
+            }
 
 
-        /*  Scanner scanner = new Scanner(System.in);
-        System.out.println("Sentence :");
-        String Sentence = scanner.nextLine();
-
-        System.out.println("Start index :");
-        int start = scanner.nextInt();
-
-        System.out.println("end index :");
-        int end = scanner.nextInt();
-
-        String result = Sentence.substring(start, end);
-        System.out.println(result);
-    }
-} */
-
-       /*  Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter a sentence:");
-        String sentence = scanner.nextLine();
-
-        System.out.print("Enter start index: ");
-        int startIndex = scanner.nextInt();
-
-        System.out.print("Enter end  index: ");
-        int endIndex = scanner.nextInt();
-
-        String result = sentence.substring(startIndex, endIndex);
-       System.out.println(result);
-
-    }
-} */
-
-       /* Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter a sentence: ");
-        String sentence = scanner.nextLine();
-
-        System.out.print("Enter a keyword: ");
-        String keyword = scanner.nextLine();
-
-        if (sentence.contains(keyword)) {
-            System.out.println("Keyword \"" + keyword + "\" is present in the sentence.");
-        } else {
-            System.out.println("Keyword \"" + keyword + "\" is not present in the sentence.");
-        }
-    }
-} */
-
-       /* Scanner scanner = new Scanner(System.in);
-
-        System.out.print("sentence: ");
-        String sentence = scanner.nextLine();
-
-        System.out.print("Replace= ");
-        String wordToReplace = scanner.nextLine();
-
-        System.out.print("Replacement Word = ");
-        String replacementWord = scanner.nextLine();
-
-        String modifiedSentence = sentence.replace(wordToReplace, replacementWord);
-
-        System.out.println("Modified sentence: " + modifiedSentence);
-    }
-} */
-
-
-
-
-       /* Scanner scanner = new Scanner(System.in);
-        System.out.println("String 1");
-        String str1 = scanner.nextLine();
-
-        System.out.println("String 2");
-        String str2 = scanner.nextLine();
-        if (str1.equalsIgnoreCase(str2)) {
-            System.out.println("Strings are aqual (ignoring case).");
-        } else {
-            System.out.println("Strings are Not aqual");
-        }
     }
 }
-*/
+
+
+//               Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println(" Enter a num :");
+//        int num = scanner.nextInt();
+//
+//        for(int i = 0 ; i <= num.)
+//
+//
+
+//        Scanner scanner = new Scanner(System.in);
+//        char a;
+//        System.out.println("string");
+//        String s = scanner.next();
+//
+//
+//        for (int i = 0; i < s.charAt(0); i++) {
+//            char a = scanner.nextInt();
+//        }  System.out.println(a);
+//    }
+//}
+
+
+
+
+
+
 
 
 
